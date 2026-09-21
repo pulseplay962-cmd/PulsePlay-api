d2a9af12d5a975c11cc629530786e6c0067e4f27
pulseplay962-cmd <pulseplay962@gmail.com>
2026-09-21 16:00:04 -0400

    Fix Express 5 CORS preflight deployment

--- server.js
+++ server.js
@@ -37,7 +37,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 
 app.use("/api/stripe", stripeWebhookRoutes);
 console.log("Stripe webhook mounted at /api/stripe/webhook");
